2e9bf48f132144b2c8dfa45216ec66046466e123
james.joung <jamesjoung8@gmail.com>
2023-06-30 19:08:10 +1000

    comments

--- Calculator/Class/Operation.cs
+++ Calculator/Class/Operation.cs
@@ -3,7 +3,7 @@ namespace Calculator.Class
 {
 	public abstract class Operation
 	{
-		public Input UserInput { get; set; }
+		public Input UserInput { get; set; } // input 자체도 opereation를 inherit해야되니깐 operation안에 input이 있으면 안되. input이 곧 operation이여야되
 		public Operation()
 		{
 			UserInput = new Input();
